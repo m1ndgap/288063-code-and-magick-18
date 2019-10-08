7aaa7b392c6255056528e9f3eca028fa932817ff
Tim Chumbash <posseslash@gmail.com>
2019-10-08 16:09:27 +0200

    removed useless arrays

--- js/stat.js
+++ js/stat.js
@@ -1,10 +1,6 @@
 'use strict';
 
-var playerNames = ['Василий', 'Аркадий', 'Эдуард'];
-var playerTimes = ['420', '69', '1337'];
-
 window.renderStatistics = function(ctx, names, times) {
-
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
   ctx.fillStyle = 'white';
